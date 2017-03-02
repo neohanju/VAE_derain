17e337e3ce6a680cd03b50720f545bda3095dcc9
Haanju Yoo <neohanju@gmail.com>
2017-03-03 02:28:03 +0900

    under constructing convolutional VAE

--- util/util.lua
+++ util/util.lua
@@ -259,7 +259,12 @@ function util.cudnn(net)
 	return cudnn_convert_custom(net, cudnn)
 end
 
-
+function util.containsValue(table, value)
+  for k, v in pairs(table) do 
+    if v == value then return true end
+  end
+  return false
+end
 
 
 
